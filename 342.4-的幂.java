18793740e66dc9bf2eac8f8f577c7993a1ddf7f0
luyongsi <luyongsi123@gmail.com>
2020-01-05 22:02:58 +0800

    1-5 周日 Signed-off-by: luyongsi <luyongsi123@gmail.com>

---
+++ 342.4-的幂.java
@@ -0,0 +1,14 @@
+/*
+ * @lc app=leetcode.cn id=342 lang=java
+ *
+ * [342] 4的幂
+ */
+
+// @lc code=start
+class Solution {
+    public boolean isPowerOfFour(int num) {
+        return (num > 0) && ((num & (num - 1)) == 0) && (num % 3 == 1);
+    }
+}
+// @lc code=end
+
